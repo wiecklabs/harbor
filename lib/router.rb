fbc3af3d6b8141ba667e824a4d914220c10f0a29
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-09-13 00:21:17 -0500

    The router. It Lives!

--- lib/router.rb
+++ lib/router.rb
@@ -1,7 +1,21 @@
+module Rack
+  class Request
+    def request_method
+      @env['REQUEST_METHOD'] = params['_method'].upcase if request_method_in_params?
+      @env['REQUEST_METHOD']
+    end
+
+    private
+    def request_method_in_params?
+      @env["REQUEST_METHOD"] == "POST" && %w(PUT DELETE).include?(params['_method'].upcase)
+    end
+  end
+end
+
 class Router
 
   URI_CHAR = '[^/?:,&#\.]'.freeze unless defined?(URI_CHAR)
-  PARAM = /(:(#{URI_CHAR}+)|\*)/.freeze unless defined?(PARAM)
+  PARAM = /(:(#{URI_CHAR}+))/.freeze unless defined?(PARAM)
 
   attr_accessor :routes
 
@@ -9,8 +23,23 @@ class Router
     @routes = []
   end
 
+  # Matches a GET request
   def get(matcher, &handler)
     register(:get, matcher, &handler)
+  end
+
+  # Matches a POST (create) request
+  def post(matcher, &handler)
+    register(:post, matcher, &handler)
+  end
+
+  # Matches a PUT (update) request
+  def put(matcher, &handler)
+    register(:put, matcher, &handler)
+  end
+
+  def delete(matcher, &handler)
+    register(:delete, matcher, &handler)
   end
 
   def register(request_method, matcher, &handler)
@@ -19,7 +48,9 @@ class Router
 
   def match(request)
     @routes.each do |request_method, matcher, handler|
-      break handler if request.request_method == request_method && matcher.call(request)
+      next unless request.request_method == request_method
+      next unless matcher.call(request)
+      break handler
     end
   end
 
@@ -29,6 +60,15 @@ class Router
     case matcher
     when Proc then matcher
     when Regexp then lambda { |request| request.path_info =~ matcher }
+    when String
+      param_keys = []
+      regex = matcher.gsub(PARAM) { param_keys << $2; "(#{URI_CHAR}+)" }
+      regex = /#{regex}/
+      lambda do |request|
+        if request.path_info =~ regex
+          request.params.update(Hash[param_keys.zip($~.captures)])
+        end
+      end
     end
   end
 
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -4,6 +4,14 @@ require "pathname"
 require "rack/request"
 require Pathname(__FILE__).dirname.parent + "lib/router"
 
+module Rack
+  class Request
+    def params
+      @params ||= {}
+    end
+  end
+end
+
 describe "Router" do
 
   describe "#register" do
@@ -19,31 +27,40 @@ describe "Router" do
     end
   end
 
-  describe "#match" do
-    before :all do
+  describe "#transform" do
+    before do
       @router = Router.new
-      @router.register :get, lambda { |request| request.path_info == "/users/show" } do
-        "User"
+      @request = Rack::Request.new("PATH_INFO" => "/users", "REQUEST_METHOD" => "GET")
+    end
+
+    describe "with a proc" do
+      it "should do nothing" do
+        @router.register(:get, lambda { |r| r.path_info == "/users" }) { "Root" }
+        @router.match(@request).call.should == "Root"
       end
     end
 
-    it "should match the correct route" do
-      request = Rack::Request.new("PATH_INFO" => "/users/show", "REQUEST_METHOD" => "GET")
-      @router.match(request).call.should == "User"
+    describe "with a regular expression" do
+      it "should match the expression on request.path_info" do
+        @router.register(:get, /^\/users$/) { "Root" }
+        @router.match(@request).call.should == "Root"
+      end
     end
 
-  end
+    describe "with a string" do
+      it "should transform it to a regular expression" do
+        @router.register(:get, "/:controller") { "Root" }
+        @router.match(@request).call.should == "Root"
+      end
 
-  describe "#get" do
-    before :all do
-      @router = Router.new
-      @request = Rack::Request.new("PATH_INFO" => "/users/show", "REQUEST_METHOD" => "GET")
+      it "should update the request params" do
+        @router.register(:get, "/:controller") { "Root" }
+        @router.match(@request)
+        @request.params.should include("controller")
+        @request.params["controller"].should == "users"
+      end
     end
 
-    it "should transform a regex to a block" do
-      @router.get(/\/users\/.*/) { "User" }
-      @router.match(@request).call.should == "User"
-    end
   end
 
 end
