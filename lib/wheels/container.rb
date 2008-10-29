1d6f5960dccde1a189c78f5ef4c81d9a92c0d573
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-10-29 18:46:24 -0500

    Fixing ugly spaces.

--- lib/wheels/container.rb
+++ lib/wheels/container.rb
@@ -2,46 +2,46 @@ require 'set'
 
 module Wheels
   class Container
-  
+
     def initialize
       @services = {}
       @dependencies = {}
     end
-  
+
     def get(name, optional_properties = {})
       raise ArgumentError.new("#{name} is not a registered service name") unless @services.key?(name)
       service = @services[name].new
       dependencies(name).each do |dependency|
         service.send("#{dependency}=", get(dependency, optional_properties))
       end
-    
+
       optional_properties.each_pair do |k,v|
         writer = "#{k}="
         service.send(writer, v) if service.respond_to?(writer)
       end
-    
+
       service
     end
-  
+
     def register(name, type)
       type_dependencies = dependencies(name)
       type_methods = type.instance_methods.grep(/\=$/)
-    
-      @services.each do |service_name, service_type|      
+
+      @services.each do |service_name, service_type|
         if service_type.instance_methods.include?("#{name}=")
           dependencies(service_name) << name
         end
-      
+
         if type_methods.include?("#{service_name}=")
           type_dependencies << service_name
         end
       end
-    
+
       @services[name] = type
-    
+
       self
     end
-  
+
     private
     def dependencies(service)
       @dependencies[service] ||= Set.new
