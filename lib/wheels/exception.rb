44231a39a0139d3c43356c2857f3a0adbe7ad325
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-12-02 14:51:13 -0600

    Adding in Wheels::Exception handling.

---
+++ lib/wheels/exception.rb
@@ -0,0 +1,57 @@
+module Wheels
+  class Exception < Rack::ShowExceptions
+    def initialize(app, layout = "layouts/exception")
+      @app = app
+      @environment = ENV["ENVIRONMENT"] || "development"
+      @template = ERB.new(TEMPLATE) if development?
+      @layout = layout
+    end
+
+    def call(env)
+      result = @app.call(env)
+      result[0] == 404 ? render_404(env) : result
+    rescue StandardError, LoadError, SyntaxError => e
+      if development?
+        [500, {"Content-Type" => "text/html"}, pretty(env, e)]
+      else
+        trace = ""
+        trace << "="*80
+        trace << "\n"
+        trace << "== [ #{e} @ #{Time.now} ] =="
+        trace << "\n"
+        trace << e.backtrace.join("\n")
+        trace << "\n"
+        trace << "== [ Request ] =="
+        trace << "\n"
+        trace << Rack::Request.new(env).env.to_yaml
+        trace << "\n"
+        trace << "="*80
+        trace << "\n"
+
+        # Add trace to log.
+        puts trace
+
+        m = Wheels::Mailer.new
+        m.mail_server = Wheels::SendmailServer.new
+        m.from = "exceptions@wieck.com"
+        m.to = "dev@wieck.com"
+        m.subject = "[ERROR] [#{ENV["USER"]}] #{e}"
+        m.text = trace
+        m.set_header("X-Priority", 1)
+        m.set_header("X-MSMail-Priority", "High")
+        m.send!
+
+        [500, {"Content-Type" => "text/html"}, Wheels::View.new("exceptions/500.html.erb", :request => Wheels::Request.new(@app, env)).to_s(@layout)]
+      end
+    end
+
+    def development?
+      @environment == "development"
+    end
+
+    private
+    def render_404(env)
+      [404, {"Content-Type" => "text/html"}, Rack::Response.new(Wheels::View.new("exceptions/404.html.erb", :request => Wheels::Request.new(@app, env)).to_s(@layout), 404)]
+    end
+  end
+end
