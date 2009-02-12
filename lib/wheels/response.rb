f6467eb611ea543c39083bebe7fbe103727bcf5a
Mike Mayo <wieck@desk5.wieck.com>
2009-02-12 16:13:11 -0600

    added to_s to the path when using xsendfile

--- lib/wheels/response.rb
+++ lib/wheels/response.rb
@@ -47,7 +47,7 @@ module Wheels
     
     def send_file(name, path, content_type = Rack::File::MIME_TYPES.fetch(File.extname(path)[1..-1], "binary/octet-stream"))
       if @request.env.has_key?("HTTP_X_SENDFILE_TYPE")
-        @headers["X-Sendfile"] = path
+        @headers["X-Sendfile"] = path.to_s
         @headers["Content-Length"] = File.size(path)
       else
         @io = BlockIO.new(path)
