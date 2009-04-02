9c38b81f5bb3e0c0275f4404448fdc8fe88be549
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-04-02 15:07:15 -0500

    Controllers now get their own logger, logging by default to log/app.log

--- lib/wheels/logging.rb
+++ lib/wheels/logging.rb
@@ -2,14 +2,26 @@ require "logging"
 
 Logging.configure do
 
+  logger 'root' do
+    level :debug
+    appenders 'app'
+  end
+
   logger 'request' do
+    additive false
     level :info
     appenders 'request'
   end
 
   logger 'error' do
+    additive false
     level :error
     appenders 'error'
+  end
+
+  appender 'app' do
+    type 'File'
+    filename 'log/app.log'
   end
 
   appender 'request' do
