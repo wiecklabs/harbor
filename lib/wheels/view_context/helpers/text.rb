09bb21acb04bfcb6ca850d848bcaf8a8125f2410
Sam Smoot <ssmoot@gmail.com>
2009-04-05 15:04:52 -0500

    Added TODO comments, incremented version number.

--- lib/wheels/view_context/helpers/text.rb
+++ lib/wheels/view_context/helpers/text.rb
@@ -6,15 +6,18 @@ module Wheels::ViewContext::Helpers::Text
   
   # Querystring escape +value+
   def q(value)
+    # TODO: Remove external dependency!
     Rack::Utils::escape(value)
   end
 
   # HTML escape +value+
   def h(value)
+    # TODO: Remove external dependency!
     Rack::Utils::escape_html(value)
   end
   
   # Truncate long text values
+  # TODO: Test!
   def truncate(value, character_count = 30, trailing = "&hellip;")
     raise ArgumentError.new("Wheels::ViewContext::Helpers::Text#truncate[character_count] must be an Integer") unless character_count.is_a?(Integer)
     raise ArgumentError.new("Wheels::ViewContext::Helpers::Text#truncate[trailing] must be a String") unless trailing.is_a?(String)
@@ -37,10 +40,9 @@ module Wheels::ViewContext::Helpers::Text
     end
   end
   
-  # Truncate long text values on the nearest word.
-  def truncate_on_words(value, word_count, trailing = "&hellip;")
-    
-    
+  # Truncate long text values on the nearest word (rounding down if the size exceeds the character_count by more than 5%).
+  # TODO: Test!
+  def truncate_on_words(value, character_count, trailing = "&hellip;")
     word_count > size ? self.strip + '...' : self.split(/ /).first(word_count).join(' ').strip + ((self.split(/ /).size > word_count) ? '...' : '')
   end
   
