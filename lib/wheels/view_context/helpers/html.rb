9ecf247c209be6602d56767670ea563133193119
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-04-06 16:07:01 -0500

    Adding Html#split_into_groups

--- lib/wheels/view_context/helpers/html.rb
+++ lib/wheels/view_context/helpers/html.rb
@@ -3,11 +3,19 @@ module Wheels::ViewContext::Helpers::Html
   ##
   # Takes a flat array and yields the data properly separated into columns.
   ##
-  def split_into_columns(data, columns = 2) #:yields: column
+  def split_into_columns(data, columns) #:yields: column
     per_column = (data.size / columns.to_f).ceil
 
     columns.times do |i|
       yield data[i*per_column, per_column]
+    end
+  end
+
+  def split_into_groups(data, groups) #:yields: group
+    rows = (data.size / groups.to_f).ceil
+
+    rows.times do |i|
+      yield data[i*groups, groups]
     end
   end
 
