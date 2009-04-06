3378f33ea69a19f961b086a329a272dc2e3717d0
Sam Smoot <ssmoot@gmail.com>
2009-04-06 10:56:38 -0500

    Tested Helpers::Text#truncate.

--- lib/wheels/view_context/helpers/text.rb
+++ lib/wheels/view_context/helpers/text.rb
@@ -3,7 +3,7 @@
 # or truncating (previewing) long pieces of text (such as photo captions).
 ##
 module Wheels::ViewContext::Helpers::Text
-  
+
   # Querystring escape +value+
   def q(value)
     # TODO: Remove external dependency!
@@ -15,19 +15,33 @@ module Wheels::ViewContext::Helpers::Text
     # TODO: Remove external dependency!
     Rack::Utils::escape_html(value)
   end
-  
+
   # Truncate long text values
-  # TODO: Test!
   def truncate(value, character_count = 30, trailing = "&hellip;")
-    raise ArgumentError.new("Wheels::ViewContext::Helpers::Text#truncate[character_count] must be an Integer") unless character_count.is_a?(Integer)
-    raise ArgumentError.new("Wheels::ViewContext::Helpers::Text#truncate[trailing] must be a String") unless trailing.is_a?(String)
-    
+    unless character_count.is_a?(Integer)
+      raise ArgumentError.new(
+        "Wheels::ViewContext::Helpers::Text#truncate[character_count] must be an Integer, was #{character_count.inspect}"
+      )
+    end
+
+    unless character_count > 0
+      raise ArgumentError.new(
+        "Wheels::ViewContext::Helpers::Text#truncate[character_count] must be greater than zero, was #{character_count.inspect}."
+      )
+    end
+
+    unless trailing.is_a?(String)
+      raise ArgumentError.new(
+        "Wheels::ViewContext::Helpers::Text#truncate[trailing] must be a String, was #{trailing.inspect}"
+      )
+    end
+
     if value.nil?
       ""
     else
       string_form = value.to_s
-      
-      if string_form.empty?
+
+      if string_form.nil? || string_form.empty?
         ""
       elsif string_form.size <= character_count
         string_form
@@ -39,11 +53,11 @@ module Wheels::ViewContext::Helpers::Text
       end
     end
   end
-  
+
   # Truncate long text values on the nearest word (rounding down if the size exceeds the character_count by more than 5%).
   # TODO: Test!
   def truncate_on_words(value, character_count, trailing = "&hellip;")
     word_count > size ? self.strip + '...' : self.split(/ /).first(word_count).join(' ').strip + ((self.split(/ /).size > word_count) ? '...' : '')
   end
-  
+
 end
---
+++ test/text_helper_test.rb
@@ -0,0 +1,80 @@
+require "helper"
+
+class TextHelperTest < Test::Unit::TestCase
+
+  class TextHelper
+    include Wheels::ViewContext::Helpers::Text
+  end
+
+  def setup
+    @helper = TextHelper.new
+    @value = <<-EOS.strip.split.map { |line| line.strip }.join(" ")
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    EOS
+  end
+
+  # truncate(value, character_count = 30, trailing = "&hellip;")
+  def test_truncate_at_defaults
+    assert_equal(@value[0, 29] + "&hellip;", @helper.truncate(@value))
+  end
+
+  def test_truncate_at_20_characters
+    assert_equal(@value[0, 19] + "&hellip;", @helper.truncate(@value, 20))
+  end
+
+  def test_truncate_at_50_characters_and_three_periods
+    assert_equal(@value[0, 47] + "...", @helper.truncate(@value, 50, "..."))
+  end
+
+  def test_truncate_no_op
+    assert_equal(@value, @helper.truncate(@value, 500))
+    assert_equal(@value, @helper.truncate(@value, 500, "..."))
+  end
+
+  def test_arbitrary_objects
+    user = Class.new do
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+      end
+
+      def to_s
+        @name
+      end
+    end
+
+    bob = user.new("Bob is a really cool guy who is always on time, unlike Mike who is a slacker.")
+
+    assert_equal("Bob is a really", @helper.truncate(bob, 15, ""))
+  end
+
+  def test_truncate_blank_values
+    # Empty or Nil input should not error.
+    assert_nothing_raised do
+      assert_equal("", @helper.truncate(""))
+    end
+
+    assert_nothing_raised do
+      assert_equal("", @helper.truncate(nil))
+    end
+  end
+
+  def test_truncate_errors
+    assert_raise(ArgumentError) do
+      # character_count should be a non-zero number.
+      assert_equal("", @helper.truncate("...", 0))
+    end
+
+    assert_raise(ArgumentError) do
+      # character_count should be non-nil.
+      assert_equal("", @helper.truncate("", nil))
+    end
+  end
+
+end
