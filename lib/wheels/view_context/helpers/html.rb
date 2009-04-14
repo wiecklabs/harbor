78c567ef091a368bb975e0f6dbb244fcb91dc0a0
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-04-14 14:47:59 -0500

    Adding protection to split_into_columns/groups when data is empty.

--- lib/wheels/view_context/helpers/html.rb
+++ lib/wheels/view_context/helpers/html.rb
@@ -4,6 +4,8 @@ module Wheels::ViewContext::Helpers::Html
   # Takes a flat array and yields the data properly separated into columns.
   ##
   def split_into_columns(data, columns) #:yields: column
+    return if data.empty?
+
     per_column = (data.size / columns.to_f).ceil
 
     columns.times do |i|
@@ -12,6 +14,8 @@ module Wheels::ViewContext::Helpers::Html
   end
 
   def split_into_groups(data, groups) #:yields: group
+    return if data.empty?
+
     rows = (data.size / groups.to_f).ceil
 
     rows.times do |i|
