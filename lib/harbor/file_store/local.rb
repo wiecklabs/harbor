aebcc9d54c7e355e8290877f6190fbde7a7fb875
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-06-19 12:16:57 -0500

    Adding cache size maintenance to local file store.

--- lib/harbor/file_store/local.rb
+++ lib/harbor/file_store/local.rb
@@ -8,6 +8,8 @@ module Harbor
         @path = Pathname(path)
 
         @options = options
+
+        ::FileUtils.mkdir_p(path.to_s)
       end
 
       def get(path)
@@ -16,6 +18,9 @@ module Harbor
 
       def put(path, file)
         f = Harbor::FileStore::File.new(self, path)
+
+        size = file.is_a?(::File) ? ::File.size(file) : file.size
+        cleanup(size)
 
         ::FileUtils.mkdir_p((@path + path).parent.to_s) unless (@path + path).parent.exist?
 
@@ -44,6 +49,32 @@ module Harbor
       end
 
       def local?
+        true
+      end
+
+      private
+
+      def __size__
+        `du -sk #{Shellwords.escape(@path.to_s)} | awk '{ print $1; }'`.chomp.to_i * 1024
+      end
+
+      def cleanup(space = nil)
+        size = __size__
+        size -= space if space # make room for a specified file size if specified
+
+        return false unless @options[:cache_size] && size > @options[:cache_size]
+
+        Dir[@path + "**/*"].each do |file|
+          next unless ::File.ctime(file) < Time.now - (@options[:cache_time] || 60)
+
+          file = Pathname(file).relative_path_from(@path)
+
+          size -= size(file)
+          delete(file)
+
+          break if size < @options[:cache_size]
+        end
+
         true
       end
 
--- test/file_store/local_file_store_test.rb
+++ test/file_store/local_file_store_test.rb
@@ -6,11 +6,11 @@ require "harbor/file_store"
 class LocalFileStoreTest < Test::Unit::TestCase
 
   def setup
-    @tmp = Harbor::FileStore::Local.new("/tmp")
+    @tmp = Harbor::FileStore::Local.new("/tmp/__file_store_test__")
   end
 
   def teardown
-    FileUtils.rm("/tmp/__local_file_store_test__") if File.exists?("/tmp/__local_file_store_test__")
+    FileUtils.rm("/tmp/__file_store_test__/__local_file_store_test__") if File.exists?("/tmp/__file_store_test__/__local_file_store_test__")
   end
 
   def test_put_with_file
@@ -21,7 +21,7 @@ class LocalFileStoreTest < Test::Unit::TestCase
       @tmp.put(filename, file)
     end
 
-    assert(File.exists?("/tmp/#{filename}"), "File should exist: /tmp/#{filename}")
+    assert(File.exists?("/tmp/__file_store_test__/#{filename}"), "File should exist: /tmp/__file_store_test__/#{filename}")
   end
 
   def test_put_with_directory_structure
@@ -32,10 +32,10 @@ class LocalFileStoreTest < Test::Unit::TestCase
       @tmp.put(filename, file)
     end
 
-    assert(File.exists?("/tmp/#{filename}"))
+    assert(File.exists?("/tmp/__file_store_test__/#{filename}"))
   ensure
-    FileUtils.rm("/tmp/#{filename}")
-    Harbor::File.rmdir_p("/tmp/__dir__")
+    FileUtils.rm("/tmp/__file_store_test__/#{filename}")
+    Harbor::File.rmdir_p("/tmp/__file_store_test__/__dir__")
   end
 
   def test_delete_with_directory_structure
@@ -46,11 +46,11 @@ class LocalFileStoreTest < Test::Unit::TestCase
     @tmp.delete(filename)
 
     assert(!@tmp.exists?(filename))
-    assert(!File.exists?("/tmp/#{filename}"))
-    assert(!File.exists?("/tmp/__dir__"))
+    assert(!File.exists?("/tmp/__file_store_test__/#{filename}"))
+    assert(!File.exists?("/tmp/__file_store_test__/__dir__"))
   ensure
-    FileUtils.rm("/tmp/#{filename}") rescue nil
-    Harbor::File.rmdir_p("/tmp/__dir__") rescue nil
+    FileUtils.rm("/tmp/__file_store_test__/#{filename}") rescue nil
+    Harbor::File.rmdir_p("/tmp/__file_store_test__/__dir__") rescue nil
   end
 
   def test_delete
@@ -88,6 +88,45 @@ class LocalFileStoreTest < Test::Unit::TestCase
 
   def test_is_local
     assert(@tmp.local?)
+  end
+
+  def test_cleanup_with_no_cache_size_option
+    assert_equal(false, @tmp.send(:cleanup))
+  end
+
+  def test_cleanup_with_less_usage_than_cache_size
+    @tmp.options[:cache_size] = 1024 * 1024 * 1024
+    assert_equal(false, @tmp.send(:cleanup))
+  ensure
+    @tmp.options[:cache_size] = nil
+  end
+
+  def test_cleanup_with_more_usage_than_cache_size_but_within_cache_time
+    @tmp.options[:cache_size] = 20
+
+    File.open(@tmp.path + "__testing_cleanup__", "w") { |f| f.write('a' * 120) }
+
+    assert(@tmp.send(:cleanup))
+    assert(@tmp.exists?("__testing_cleanup__"))
+  ensure
+    @tmp.options[:cache_size] = nil
+    File.rm(@tmp.path + "__testing_cleanup__") rescue nil
+  end
+
+  def test_cleanup_with_more_usage_than_cache_size_and_outside_cache_time
+    @tmp.options[:cache_size] = 20
+    @tmp.options[:cache_time] = 1
+
+    File.open(@tmp.path + "__testing_cleanup__", "w") { |f| f.write('a' * 120) }
+
+    sleep(2)
+
+    assert(@tmp.send(:cleanup))
+    assert(!@tmp.exists?("__testing_cleanup__"))
+  ensure
+    @tmp.options[:cache_size] = nil
+    @tmp.options[:cache_time] = nil
+    File.rm(@tmp.path + "__testing_cleanup__") rescue nil
   end
 
 end
