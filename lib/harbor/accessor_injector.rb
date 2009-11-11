68a8adcea98d3bd9ed08d1441921074786af9a46
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-11-11 03:22:47 -0600

    [DOC] Adding documentation for Harbor::AccessorInjector

--- lib/harbor/accessor_injector.rb
+++ lib/harbor/accessor_injector.rb
@@ -1,4 +1,20 @@
 module Harbor
+  ##
+  # A simple module to facilitate accessor based injection. This is used by Harbor::Plugin
+  # to allow you to easily construct plugins from hashes, i.e.:
+  # 
+  #   <%= plugin("user/tabs", :user => @user)
+  # 
+  # It can of course be used 
+  # 
+  #   class Dog
+  #     include Harbor::AccessorInjector
+  #     attr_accessor :owner
+  #   end
+  # 
+  #   dog = Dog.new.inject(:owner => "Tom")
+  #   dog.owner # => "Tom"
+  ##
   module AccessorInjector
 
     def inject(options = {})
