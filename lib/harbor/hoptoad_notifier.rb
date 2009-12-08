ef064a45b0e83e902262b59080442067400d9421
keith <keith@development.lan>
2009-12-08 02:29:17 -0600

    Adding Hoptoad Notifier

---
+++ lib/harbor/hoptoad_notifier.rb
@@ -0,0 +1,47 @@
+##
+# Utility class for sending hoptoad notifications of exceptions in
+# non-development environments.
+# 
+#   require 'harbor/hoptoad_notifier'
+#   Harbor::HoptoadNotifier.hoptoad_api_key = "YOUR_API_KEY_HERE"
+# 
+#   Your API key can be found under Projects -> Project Name -> Edit this project -> Current API Key
+##
+require 'lilypad'
+
+class Lilypad
+  class <<self
+    def production?
+      Config.environments.include? ENV['ENVIRONMENT']
+    end
+  end
+end
+
+module Harbor
+  class HoptoadNotifier
+    def self.hoptoad_api_key=(api_key)
+      @@api_key = api_key
+    end
+
+    def self.hoptoad_api_key
+      @@api_key
+    rescue NameError
+      raise "Harbor::HoptoadNotifier.api_key not set."  
+    end
+
+    def self.hoptoad_api_key?
+      defined?(@@api_key)
+    end
+
+    def self.notify(exception, request, response, trace)
+      require 'ruby-debug'
+      debugger
+
+      Lilypad.config self.hoptoad_api_key
+      
+      Lilypad.notify(exception, request.env)
+    end
+  end
+end
+
+Harbor::Application.register_event(:exception) { |*args| Harbor::HoptoadNotifier.notify(*args) }
--- lib/harbor/version.rb
+++ lib/harbor/version.rb
@@ -1,3 +1,3 @@
 module Harbor
-  VERSION = "0.16.7"
+  VERSION = "0.16.8"
 end
