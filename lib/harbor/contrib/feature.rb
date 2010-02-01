489c10bfaca7a2b414ab4da40e7000b9fd5bdc35
Bernerd Schaefer <bj.schaefer@gmail.com>
2010-02-01 13:35:26 -0600

    [VERSION 0.18] Adding Harbor::Contrib::Feature
    Contrib::Feature is a convenience class for bundling optional features
    into Harbor applications.

---
+++ lib/harbor/contrib/feature.rb
@@ -0,0 +1,60 @@
+module Harbor
+  module Contrib
+    ##
+    # Convenience class for bundling optional features in Harbor applications.
+    # 
+    #   class MyApplication::Features::Feature < Harbor::Contrib::Feature
+    #     class << self
+    #       attr_accessor :option
+    #     end
+    # 
+    #     def self.enable
+    #       if enabled = super
+    #         require "necessary/files"
+    #       end
+    #       
+    #       enabled
+    #     end
+    #   end
+    # 
+    #   MyApplication::Feature::Feature.enable do |feature|
+    #     feature.option = true
+    #   end
+    # 
+    # It can also be helpful to utilize Harbor::Router#merge! to bundle routes with
+    # the feature.
+    # 
+    #   class MyApplication::Features::Feature
+    #     def self.routes(services)
+    #       Harbor::Router.new do
+    #         get("/feature") { |request, response| response.puts "Inside feature" }
+    #       end
+    #     end
+    #   end
+    # 
+    #   class MyApplication
+    #     def self.routes(services)
+    #       Harbor::Router.new do
+    #         get("/") { |request, response| response.puts "Inside MyApplication" }
+    #         merge!(Features::Feature.routes(services)) if Features::Feature.enabled?
+    #       end
+    #     end
+    # 
+    ##
+    class Feature
+      @@enabled = false
+      def self.enable
+        return false if @@enabled
+        @@enabled = true
+
+        yield self if block_given?
+
+        @@enabled
+      end
+
+      def self.enabled?
+        @@enabled
+      end
+    end
+  end
+end
--- lib/harbor/version.rb
+++ lib/harbor/version.rb
@@ -1,3 +1,3 @@
 module Harbor
-  VERSION = "0.17.3"
+  VERSION = "0.18"
 end
