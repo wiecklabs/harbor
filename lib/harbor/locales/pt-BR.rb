027d4fe68bc9eb81d693df5dffcada92ca69bc96
Fábio Rehm <fgrehm@gmail.com>
2010-04-23 20:09:19 -0300

    pt and pt-BR locales

--- lib/harbor/locales/pt-BR.rb
+++ lib/harbor/locales/pt-BR.rb
@@ -1,8 +1,8 @@
 locale = Harbor::Locale.new
-locale.culture_code        = 'pt'
-locale.time_formats        = {:long => "%d-%m-%Y %H:%m:%s", :default => "%H:%m:%s"}
-locale.date_formats        = {:default => '%d-%m-%Y'}
-locale.decimal_formats     = {:default => "%8.2f", :currency => "€%8.2f"}
+locale.culture_code        = 'pt-BR'
+locale.time_formats        = {:long => "%d/%m/%Y %H:%m:%s", :default => "%H:%m:%s"}
+locale.date_formats        = {:default => '%d/%m/%Y'}
+locale.decimal_formats     = {:default => "%8.2f", :currency => "R$%8.2f"}
 locale.wday_names          = [nil, 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 locale.wday_abbrs          = [nil, 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 locale.month_names         = [nil, 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
