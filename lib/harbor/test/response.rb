e00a13f6fb09b4bbc0adba33e8862d5e8fdcddad
Nicholas <nicholas.d.phillips@gmail.com>
2010-05-17 19:14:39 -0500

    Allow Test::Response access to the context.

--- lib/harbor/test/response.rb
+++ lib/harbor/test/response.rb
@@ -2,14 +2,30 @@ module Harbor
   module Test
     class Response < Harbor::Response
 
-      attr_accessor :request
+      attr_accessor :request, :context
 
       ##
       # We redefine Harbor::Response.initialize(request) with an empty arg
       # variant for use with a container.
       ##
       def initialize
+        self.context = []
         super(nil)
+      end
+
+      ##
+      # Gives us access to the context, so instance variables can be assessed
+      ##
+      def render(view, context = {})
+        self.context << context
+        super(view, context)
+      end
+
+      ##
+      # Cycle handles multi-render actions
+      ##
+      def render_context(cycle = 0)
+        context[cycle]
       end
 
     end
