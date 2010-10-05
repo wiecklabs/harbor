d95da7327fa40b862a606bc0629db9d74bc73bab
Mike <mike@copernicus.local>
2010-10-05 17:37:17 -0500

    Added filtering for stats to allow assignable denied_users and bot agents.  PageViews and UserAgents are not recorded for offenders.  Bumped to 0.18.47

--- lib/harbor/contrib/stats.rb
+++ lib/harbor/contrib/stats.rb
@@ -14,6 +14,61 @@ module Harbor
       def self.orm
         @orm
       end
+      
+      def self.denied_ips
+        @@denied_ips ||= []
+      end
+      
+      def self.denied_ips=(ips)
+        @@denied_ips = ips
+      end
+      
+      def self.known_bots
+        @@bots ||= [
+          /yahoo.*slurp/i,
+          /googlebot/i,
+          /msnbot/i,
+          /charlotte.*searchme/i,
+          /twiceler.*robot/i,
+          /dotbot/i,
+          /gigabot/i,
+          /yanga.*bot/i,
+          /gaisbot/i,
+          /becomebot/i,
+          /yandex/i,
+          /catchbot/i,
+          /cazoodlebot/i,
+          /jumblebot/i,
+          /librabot/i,
+          /jyxobot/i,
+          /mlbot/i,
+          /cipinetbot/i,
+          /funnelbot/i,
+          /mj12bot/i,
+          /spinn3r/i,
+          /nutch.*bot/i,
+          /oozbot/i,
+          /robotgenius/i,
+          /snapbot/i,
+          /tmangobot/i,
+          /yacybot/i,
+          /rpt.*httpclient/i,
+          /indy.*library/i,
+          /baiduspider/i,
+          /WhistleBlower/i,
+          /Pingdom/,
+          /discobot/i,
+          /charlotte.*searchme/i
+        ]
+      end
+      
+      def self.known_bots=(bots)
+        @@bots = bots
+      end
+      
+      def self.denied_user?(remote_ip, user_agent)
+        known_bots.any? { |bot| user_agent =~ bot } || denied_ips.include?(remote_ip)
+      end
 
     end
   end
@@ -21,7 +76,7 @@ end
 
 Harbor::Session.register_event_handler(:session_created) do |event|
   if orm = Harbor::Contrib::Stats.orm
-    orm::UserAgent.create(event.session_id, event.remote_ip, event.user_agent)
+    orm::UserAgent.create(event.session_id, event.remote_ip, event.user_agent) unless Harbor::Contrib::Stats.denied_user?(event.remote_ip, event.user_agent)
   else    
     warn "Harbor::Contrib::Stats::orm must be set to generate statistics."
   end
@@ -35,7 +90,9 @@ Harbor::Application.register_event_handler(:request_complete) do |event|
       session = request.session
 
       # We only record a PageView if we get a 200 and it's an actual page rendering, not providing an image or downloading a file
-      orm::PageView.create(session.id, request.uri, request.referrer) if %w(text/html text/xml text/json).include?(response.content_type) && response.status == 200
+      unless Harbor::Contrib::Stats.denied_user?(request.ip, request.env["HTTP_USER_AGENT"])
+        orm::PageView.create(session.id, request.uri, request.referrer) if %w(text/html text/xml text/json).include?(response.content_type) && response.status == 200
+      end
     end
   else    
     warn "Harbor::Contrib::Stats::orm must be set to generate statistics."
--- lib/harbor/version.rb
+++ lib/harbor/version.rb
@@ -1,3 +1,3 @@
 module Harbor
-  VERSION = "0.18.46"
+  VERSION = "0.18.47"
 end
