2875d2ae4df2bc78a670a3e5e8005358df6a0dd8
ckrailo <krailo@wieck.com>
2011-05-19 19:05:34 -0500

    add email To address overriding (useful for making sure emails on dev/stage dont go out to clients like they should on production)

--- lib/harbor/mail_servers/sendmail.rb
+++ lib/harbor/mail_servers/sendmail.rb
@@ -3,12 +3,15 @@ module Harbor
     class Sendmail < Abstract
       def initialize(config = {})
         @sendmail = config[:sendmail] || `which sendmail`.chomp
+        @filter = config[:delivery_address_filter]
       end
 
       def deliver(message_or_messages)
         messages = Array === message_or_messages ? message_or_messages : [message_or_messages]
 
         messages.each do |message|
+          @filter.apply(message) if @filter
+
           sendmail = ::IO.popen("#{@sendmail} -i -t", "w+")
           sendmail.puts message.to_s
           sendmail.close
