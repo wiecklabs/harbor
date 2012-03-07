e533dd34ea6261a82ecfb811a4bef74433147dad
Bauerpauer <bauer.mail@gmail.com>
2012-03-06 22:55:09 -0600

    Adds tests for Harbor::Commands::Setup. Fixes Erubis-generated templates for the default app skeleton.  Removes requirement that Erubis-generated skeleton templates be named w/ the .skel suffix in favor of detecting <@

--- test/harbor_setup_command_test.rb
+++ test/harbor_setup_command_test.rb
@@ -3,20 +3,31 @@ require "harbor/commands/setup"
 
 class HarborSetupCommandTest < MiniTest::Unit::TestCase
 
-  def setup
-    @temp_path = File.join(File.dirname(__FILE__), "tmp")
+  def after_tests
+    destroy_petshop if @petshop_generated
   end
 
-  def teardown
-    FileUtils.rm_rf(@temp_path) if File.directory?(@temp_path)
+  def setup
+    generate_petshop unless @petshop_generated
+  end
+
+  def generate_petshop
+    @temp_path = ::File.join(::File.dirname(__FILE__), "tmp")
+    destroy_petshop
+
+    @app_root = ::File.join(@temp_path, "petshop")
+    FileUtils.mkdir_p(@app_root)
+
+    Harbor::Commands::Setup.new('petshop', @app_root, nil).run
+
+    @petshop_generated = true
+  end
+
+  def destroy_petshop
+    FileUtils.rm_rf(@temp_path) if ::File.directory?(@temp_path)
   end
 
   def test_generator_creates_expected_app_structure
-    app_root = File.join(@temp_path, "petshop")
-    FileUtils.mkdir_p(app_root)
-
-    Harbor::Commands::Setup.new('petshop', app_root, nil).run
-
     expected_generated_file_and_directory_list = %w{
       petshop/Gemfile
       petshop/config.ru
@@ -47,9 +58,58 @@ class HarborSetupCommandTest < MiniTest::Unit::TestCase
       petshop/views/home
       petshop/views/home/index.html.erb
     }
-    
-    skeleton = Dir["#{app_root}/**/*"].map { |path| path.sub "#{@temp_path}/", "" } 
+
+    skeleton = Dir["#{@app_root}/**/*"].map { |path| path.sub "#{@temp_path}/", "" }
     assert_equal expected_generated_file_and_directory_list.sort, skeleton.sort
+  end
+
+  def test_generator_creates_proper_home_rb
+    assert_generated_file_matches "controllers/home.rb", <<-RUBY
+      class Petshop
+        class Home < Harbor::Controller
+
+          index do
+            response.render "home/index"
+          end
+
+        end
+      end
+    RUBY
+  end
+
+  def test_generator_creates_proper_petshop_rb
+    assert_generated_file_matches "lib/petshop.rb", <<-RUBY
+      require "rubygems"
+      require "bundler/setup"
+      require "harbor"
+
+      config.load!(File.dirname(__FILE__).parent + "env")
+
+      class Petshop < Harbor::Application
+
+        def initialize
+          # Any code you need to initialize your application goes here.
+        end
+
+      end
+
+      require Pathname(__FILE__).dirname + "petshop/controllers/home"
+    RUBY
+  end
+
+  def test_generator_creates_proper_boot_rb
+    assert_generated_file_matches "lib/boot.rb", <<-RUBY
+      require "lib/petshop"
+    RUBY
+  end
+
+  private
+
+  def assert_generated_file_matches(relative_file_path, expected_result)
+    path = ::File.join(@app_root, relative_file_path)
+
+    assert ::File.file?(path), "Generated File \"#{path}\" not found."
+    assert_equal expected_result.margin, ::File.read(path).strip
   end
 
 end
--- test/helper.rb
+++ test/helper.rb
@@ -54,6 +54,28 @@ class Time
 
 end
 
+class String
+
+  ##
+  # Remove whitespace margin.
+  #
+  # @return [String] receiver with whitespace margin removed
+  #
+  # @api public
+  def margin
+    lines = self.dup.split($/)
+
+    min_margin = 0
+    lines.each do |line|
+      if line =~ /^(\s+)/ && (min_margin == 0 || $1.size < min_margin)
+        min_margin = $1.size
+      end
+    end
+    lines.map { |line| line.sub(/^\s{#{min_margin}}/, '') }.join($/)
+  end
+
+end
+
 
 class MiniTest::Unit::TestCase
   class MyApplication < Harbor::Application
