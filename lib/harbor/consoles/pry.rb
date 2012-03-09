47d0b485f06786997fad21f1046aa3fb34ed62e0
Sam Smoot <ssmoot@gmail.com>
2012-03-08 22:09:20 -0600

    Whitespace cleanup.

--- lib/harbor/consoles/pry.rb
+++ lib/harbor/consoles/pry.rb
@@ -3,12 +3,12 @@ require "singleton"
 module Harbor
   module Consoles
     module Pry
-      
+
       def self.start
         require "pry"
         ::Object.new.pry
       end
-      
+
     end
   end
 end
