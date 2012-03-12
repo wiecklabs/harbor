73c9c621ac99a6055a40854a29bf90c3f776184f
Fabio Rehm <fgrehm@gmail.com>
2012-03-11 23:29:47 -0300

    Split static and wildcard routes into 2 arrays on DeferredRouteCollection

--- lib/harbor/router/deferred_route_collection.rb
+++ lib/harbor/router/deferred_route_collection.rb
@@ -2,21 +2,26 @@ require 'forwardable'
 
 module Harbor
   class Router
+    # TODO: Think about a better name for this... It handles 2 "collections" internally
     class DeferredRouteCollection
-      extend ::Forwardable
-
-      # TODO: Do we really need all of this or can we just provide a #to_a / #to_ary ?
-      def_delegators :@array, :sort!, :empty?, :size, :delete_at, :slice!, :first
+      attr_reader :static_routes, :wildcard_routes
 
       def initialize
-        @array = []
+        @wildcard_routes, @static_routes = [], []
       end
 
       def <<(route)
+        routes = route.wildcard?? @wildcard_routes : @static_routes
+
         # TODO: Make sure this will scale
-        index = @array.find_index { |r| r.normalized_tokens == route.normalized_tokens }
-        @array.delete_at(index) if index
-        @array << route
+        index = routes.find_index { |r| r.normalized_tokens == route.normalized_tokens }
+        routes.delete_at(index) if index
+        routes << route
+      end
+
+      def clear
+        @wildcard_routes.clear
+        @static_routes.clear
       end
     end
   end
--- test/router/deferred_route_collection_test.rb
+++ test/router/deferred_route_collection_test.rb
@@ -6,12 +6,30 @@ module Router
       @collection = Harbor::Router::DeferredRouteCollection.new
     end
 
-    def test_replaces_duplicate_deferred_route_uppon_registration
+    def test_replaces_duplicate_wildcard_route_upon_insertion
       @collection << Harbor::Router::DeferredRoute.new([':id'], :show)
       @collection << Harbor::Router::DeferredRoute.new([':id'], :duplicate)
 
-      assert_operator @collection.size, :==, 1
-      assert_equal :duplicate, @collection.first.action
+      assert_operator @collection.wildcard_routes.size, :==, 1
+      assert_equal :duplicate, @collection.wildcard_routes.first.action
+    end
+
+    def test_replaces_static_route_upon_insertion
+      @collection << Harbor::Router::DeferredRoute.new(['posts'], :posts)
+      @collection << Harbor::Router::DeferredRoute.new(['posts'], :duplicate)
+
+      assert_operator @collection.static_routes.size, :==, 1
+      assert_equal :duplicate, @collection.static_routes.first.action
+    end
+
+    def test_clears_static_and_wildcard_routes
+      @collection << Harbor::Router::DeferredRoute.new(['posts'], :posts)
+      @collection << Harbor::Router::DeferredRoute.new(['posts', ':id'], :show)
+
+      @collection.clear
+
+      assert_predicate @collection.static_routes, :empty?
+      assert_predicate @collection.wildcard_routes, :empty?
     end
   end
 end
