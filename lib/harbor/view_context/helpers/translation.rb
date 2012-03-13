6a792ffe461e132775e4d19d5e1ae6b48f1be916
James Childers <james.childers@gmail.com>
2012-03-13 13:30:10 -0500

    Rename: Translation -> TranslationChain

--- lib/harbor/view_context/helpers/translation.rb
+++ lib/harbor/view_context/helpers/translation.rb
@@ -1,11 +1,11 @@
-require 'harbor/contrib/translations/models/translation'
+require 'harbor/contrib/translations/models/translation_chain'
 
 ##
 # Generic URL helpers, such as merging query strings.
 ##
 module Harbor::ViewContext::Helpers::Translation
 
-    @@translator ||= Harbor::Contrib::Translations::Translation.new
+    @@translator ||= Harbor::Contrib::Translations::TranslationChain.new
 
     ##
     # Takes a string to translate and interpolates args.
