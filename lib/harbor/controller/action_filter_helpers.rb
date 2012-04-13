7603120c34a49bd396ccacfb064358ff65872c69
Fabio Rehm <fgrehm@gmail.com>
2012-04-12 23:38:25 -0300

    Docs for action filters

--- lib/harbor/controller/action_filter_helpers.rb
+++ lib/harbor/controller/action_filter_helpers.rb
@@ -1,7 +1,30 @@
 class Harbor
   class Controller
-    # TODO: Documentation
-    # TODO: Make it work with inheritance
+    ##
+    # Provides a way to register code to be run before or after controller actions.
+    #
+    # Although it supports registering filter to a path that looks like a "global"
+    # route, it will only be applied to requests that get mapped to the defining
+    # controller.
+    #
+    #   class PostsController < Harbor::Controller
+    #     # Runs block before any incoming request
+    #     before :all do
+    #       # ...
+    #     end
+    #
+    #     # Calls "log" instance method before any incoming PUT request
+    #     after :all, :request_method => :put, :call => :log
+    #
+    #     # Checks for authentication on all admin requests to this controller
+    #     before '/admin/*', :call => :check_authentication
+    #
+    #     # Checks for authorization before updating or creating a new post
+    #     before ':id', :request_method => [:put, :post], :call => :check_authorization
+    #   end
+    #
+    # TODO: Make it "inheritable"
+    ##
     module ActionFilterHelpers
       def self.included(base)
         base.extend ClassMethods
