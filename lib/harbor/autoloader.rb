c0dc62e21aa16af9b822c4e9f9d628237fccbaa8
Fabio Rehm <fgrehm@gmail.com>
2012-04-22 20:44:46 -0300

    Extract autoloader methods

--- lib/harbor/autoloader.rb
+++ lib/harbor/autoloader.rb
@@ -10,57 +10,72 @@ class Harbor
       class << self
         def load(context, constant)
           if app = app_for(context)
-            suffix = app_suffix(app, context, constant)
-
-            if suffix.include?('/') && ::File.exist?(app.root + "#{suffix}.rb")
-              require app.root + suffix
-              return context.const_get constant if context.const_defined? constant
-            elsif file = Dir["#{app.root}/{lib,models}/#{suffix}.rb"].first
-              require file
-              return context.const_get constant if context.const_defined? constant
-            elsif autoloadable_module?(app.root + suffix)
-              return if Autoloader::INVALID_APP_SUFFIXES.include? suffix
-
-              mod = Module.new
-              return context.const_set constant, mod
-            end
-
-            return
-          end
-
-          Harbor.registered_applications.each do |app|
-            suffix = suffix_for(context, constant)
-
-            if file = Dir["#{app.root}/{lib,models}/#{suffix}.rb"].first
-              require file
-              return context.const_get constant if context.const_defined? constant
-            end
-
-            module_candidate = Dir["#{app.root}/{lib,models}/#{suffix}"].first
-            if !module_candidate.nil? && autoloadable_module?(module_candidate)
-              mod = Module.new
-              context.const_set constant, mod
-              return mod
-            end
-          end
-
-          config.autoloader.paths.each do |path|
-            suffix = suffix_for(context, constant)
-
-            if file = Dir["#{path}/#{suffix}.rb"].first
-              require file
-              return context.const_get constant if context.const_defined? constant
-            elsif autoloadable_module?("#{path}/#{suffix}")
-              mod = Module.new
-              context.const_set constant, mod
-              return mod
-            end
+            return load_app_constant(app, context, constant)
+          elsif const = load_app_lib_or_model_root_constant(context, constant)
+            return const
+          elsif const = load_constant_from_registered_paths(context, constant)
+            return const
           end
 
           nil
         end
 
         private
+
+        def load_constant_from_registered_paths(context, constant)
+          suffix = suffix_for(context, constant)
+
+          paths = config.autoloader.paths.map(&:to_s).join(',')
+
+          if file = Dir["{#{paths}}/#{suffix}.rb"].first
+            require file
+            return context.const_get constant if context.const_defined? constant
+          end
+
+          module_candidate = Dir["{#{paths}}/#{suffix}/"].first
+          if module_candidate && autoloadable_module?(module_candidate)
+            mod = Module.new
+            context.const_set constant, mod
+            return mod
+          end
+        end
+
+        def load_app_lib_or_model_root_constant(context, constant)
+          suffix = suffix_for(context, constant)
+
+          paths = Harbor.registered_applications.map(&:root).map(&:to_s).join(',')
+
+          if file = Dir["{#{paths}}/{lib,models}/#{suffix}.rb"].first
+            require file
+            return context.const_get constant if context.const_defined? constant
+          end
+
+          module_candidate = Dir["{#{paths}}/{lib,models}/#{suffix}/"].first
+          if module_candidate && autoloadable_module?(module_candidate)
+            mod = Module.new
+            context.const_set constant, mod
+            return mod
+          end
+        end
+
+        def load_app_constant(app, context, constant)
+          suffix = app_suffix(app, context, constant)
+
+          if suffix.include?('/') && ::File.exist?(app.root + "#{suffix}.rb")
+            require app.root + suffix
+            return context.const_get constant if context.const_defined? constant
+          elsif file = Dir["#{app.root}/{lib,models}/#{suffix}.rb"].first
+            require file
+            return context.const_get constant if context.const_defined? constant
+          elsif autoloadable_module?(app.root + suffix)
+            return if Autoloader::INVALID_APP_SUFFIXES.include? suffix
+
+            mod = Module.new
+            return context.const_set constant, mod
+          end
+
+          return
+        end
 
         def autoloadable_module?(path)
           ::Dir.exist?(path) && !Dir["{#{path}/**/*.rb}"].empty?
@@ -146,6 +161,10 @@ class Harbor
 
       def each(&block)
         @paths.each &block
+      end
+
+      def map(&block)
+        @paths.map &block
       end
     end
   end
