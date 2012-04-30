f61d355a5a4e7974cd06474a305f50cf82d83382
Fabio Rehm <fgrehm@gmail.com>
2012-04-30 00:32:42 -0300

    Make use of config.environment when running harbor server

--- lib/harbor/commands/server.rb
+++ lib/harbor/commands/server.rb
@@ -3,5 +3,5 @@ require_relative 'boot'
 if RUBY_PLATFORM =~ /java/
   require_relative "../jetty"
 else
-  Rack::Server.start(Port: 9292, config: "config.ru", environment: ENV["ENVIRONMENT"])
+  Rack::Server.start(Port: 9292, config: "config.ru", environment: config.environment)
 end
