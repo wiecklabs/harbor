1e890ffebb3249a1abf536fe76c78642c409767d
Ryan Lewis <s.crispinian@gmail.com>
2012-10-03 14:50:21 -0500

    Using infinispan for caching in dev env

--- lib/harbor/cache/infinispan.rb
+++ lib/harbor/cache/infinispan.rb
@@ -1,14 +1,18 @@
-require Pathname(__FILE__).dirname + 'cluster_validation'
-require "digest/md5"
+Dir[Pathname(__FILE__).dirname.parent + 'contrib/infinispan/jar_files/*.jar'].each { |jar| require jar }
 
-class CacheManager
-  
+require "digest/md5"
+require "java"
+
+require Pathname(__FILE__).dirname.parent + 'contrib/infinispan/cluster_validation'
+
+class Harbor::Cache::Infinispan
+
   include_package "java.lang"  
   include_package "org.infinispan.manager"
   include_package "org.infinispan.config"
   include_package "org.infinispan.configuration.global"
   include_package "org.infinispan.configuration.cache"
-  
+
   # EVICTION NOTES:
   # LIRS is the default strategy when an eviction with max-entries is set, per
   #   https://docs.jboss.org/author/display/ISPN/Eviction#Eviction-Configurationanddefaultsin5.1.x
@@ -19,25 +23,25 @@ class CacheManager
   #
   # If we want LIRS, just set the max_entries. If you don't want to enable eviction, then
   # either don't make the call to #eviction, or set max_entries to 0 instead.
-  
+
   CLUSTER_SIZE = 2
-  
+
   def initialize(cluster_name, node_id)
     @node_id = node_id
     @default = ConfigurationBuilder.new.clustering.cache_mode(CacheMode::REPL_SYNC).l1.build
-    
+
     base_port = (Digest::MD5.hexdigest(cluster_name).to_i(16) % 4000) + 26000
     System.properties["jgroups.tcp.port"] = "#{base_port}"
     System.properties["jgroups.tcpping.initial_hosts"] = "localhost[#{base_port}],localhost[#{base_port + 1}]"
-    
+
     @manager = DefaultCacheManager.new(GlobalConfigurationBuilder.default_clustered_builder.transport.cluster_name(cluster_name).rack_id(ENV["ENVIRONMENT"]).machine_id(node_id.to_s).build, @default)
-    
+
     @manager.define_configuration "translation", ConfigurationBuilder.new.read(@default).eviction.max_entries(10).build
 
     @translations = @manager.get_cache("translation")
 
   end
-  
+
   def wait_for_cluster_to_form
     if !ClusterValidation.wait_for_cluster_to_form(@manager, @node_id, CLUSTER_SIZE)
       raise StandardError.new(
@@ -49,5 +53,4 @@ class CacheManager
   def translations
     @translations
   end
-  
 end
