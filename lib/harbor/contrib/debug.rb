a450a37cd5290c1a100beceb39f3b9aab918e2dd
Ryan Lewis <s.crispinian@gmail.com>
2012-10-22 21:56:48 -0500

    Fixing styles on debug bar

--- lib/harbor/contrib/debug.rb
+++ lib/harbor/contrib/debug.rb
@@ -95,7 +95,7 @@ module Harbor
 
   #logger ul {
     margin: 0; padding: 0; list-style: none; overflow: auto;
-    width: 294px;
+    width: 293px;
     margin: 0 auto;
     -webkit-border-top-right-radius: 5px;
     -webkit-border-top-left-radius: 5px;
