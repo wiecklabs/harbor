cbce16639e31f68086da5448d08f7d76ceabc68a
ckrailo <krailo@wieck.com>
2012-12-05 16:32:07 -0600

    Harbor::MailServers::Sendgrid config option keys are symbols, not strings.

--- lib/harbor/mail_servers/sendgrid.rb
+++ lib/harbor/mail_servers/sendgrid.rb
@@ -8,20 +8,20 @@ module Harbor
         missing_keys = config.keys - required_keys
         raise ArgumentError("You must provide :#{missing_keys.join(",:")} in the Sendgrid config.") if missing_keys.any?
 
-        config['delivery_method'] ||= :smtp
-        config['address'] ||= 'smtp.sendgrid.net'
-        config['port'] ||= 587
-        config['authentication'] ||= 'plain'
-        config['enable_starttls_auto'] ||= true
+        config[:delivery_method] ||= :smtp
+        config[:address] ||= 'smtp.sendgrid.net'
+        config[:port] ||= 587
+        config[:authentication] ||= 'plain'
+        config[:enable_starttls_auto] ||= true
 
         Mail.defaults do
-          delivery_method config['delivery_method'].to_sym, { :address => config['address'].to_s,
-            :port => config['port'].to_i,
-            :domain => config['domain'].to_s,
-            :user_name => config['user_name'].to_s,
-            :password => config['password'].to_s,
-            :authentication => config['authentication'].to_s,
-            :enable_starttls_auto => config['enable_starttls_auto'] == true }
+          delivery_method config[:delivery_method].to_sym, { :address => config[:address].to_s,
+            :port => config[:port].to_i,
+            :domain => config[:domain].to_s,
+            :user_name => config[:user_name].to_s,
+            :password => config[:password].to_s,
+            :authentication => config[:authentication].to_s,
+            :enable_starttls_auto => config[:enable_starttls_auto] == true }
         end
       end
 
